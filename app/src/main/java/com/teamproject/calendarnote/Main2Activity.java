d1dc3220e018f4c7c96dd153d93ad9e6dd7f381e
abhijeetcode <abhijeetadhikary99@gmail.com>
2017-05-04 19:05:21 +0530

    Initial commit

---
+++ app/src/main/java/com/teamproject/calendarnote/Main2Activity.java
@@ -0,0 +1,37 @@
+package com.teamproject.calendarnote;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tv=(TextView)findViewById(R.id.text_view);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        //Receive the message
+        Bundle b=getIntent().getExtras();
+        String event_details=b.getString("message");
+        tv.setText(event_details);
+    }
+
+}
